bdc8b9b9b5faec9427265969f13355dcef2a5d07
Jacinta Ngwu <jacintangwu9900@gmail.com>
2023-07-07 17:46:34 +0000

    Add workcarddata.js file

---
+++ src/components/WorkCardData.js
@@ -0,0 +1,53 @@
+import Handymate from '../assests/Handymate.png';
+import Metric from '../assests/Metrics.png';
+import Shopping from '../assests/Shopping.png';
+import Leaderboard from '../assests/Leaderboard.png';
+import TvShow from '../assests/Show.png';
+import Portfolio from '../assests/Portfolio.png';
+
+const ProjectCardData = [
+  {
+    imagsrc: Handymate,
+    title: 'Handymate',
+    text: 'This is a user-friendly home service website that connects homeowners with skilled professionals for a variety of services, including electrician, plumbing, painting, cleaning etc. The platform simplifies the process by providing detailed profiles and reviews of service providers, allowing customers to make informed decisions.',
+    view: 'https://deploy--fabulous-horse-dfd6cb.netlify.app/',
+    source: 'https://github.com/JacintaNgwu/HandyMate-Frontend.git',
+  },
+  {
+    imagsrc: Metric,
+    title: 'Metric Webapp',
+    text: 'This mobile web application is designed to provide users with the ability to check and analyze a list of numeric metrics. It specifically focuses on metrics related to countries and counties in Africa. It features a user-friendly home page and a detailed page to facilitate easy navigation and access to the desired information.',
+    view: 'https://metrics-webapp-vyns.onrender.com/',
+    source: 'https://github.com/JacintaNgwu/Metrics-webapp.git',
+  },
+  {
+    imagsrc: Shopping,
+    title: 'Shopping Website',
+    text: 'This a demo website dedicated to offering a wide range of wear options. The website consists of essential pages such as the home and about pages, each featuring multiple sections and a responsive layout. Built primarily using HTML, CSS, and Vanilla JS, this website showcases the latest trends in fashion and provides an immersive shopping experience.',
+    view: 'https://jacintangwu.github.io/Jaccy-Shopping-Website/',
+    source: 'https://github.com/JacintaNgwu/Jaccy-Shopping-Website.git',
+  },
+  {
+    imagsrc: Leaderboard,
+    title: 'Leaderboard',
+    text: 'The purpose of this project is to create a leaderboard website that allows users to view the scores of players and engage with the platform by adding their own scores. Additionally, the website includes a form that enables users to add new players to the leaderboard, with the data being stored locally to ensure its preservation.',
+    view: 'https://jacintangwu.github.io/Leaderboard/dist/',
+    source: 'https://github.com/JacintaNgwu/Leaderboard',
+  },
+  {
+    imagsrc: TvShow,
+    title: 'TV Show Website',
+    text: 'This project entails the development of a TV show website where users can browse popular TV shows organized by different categories. Users can interact with the website by liking their favorite shows and leaving comments. The website aims to create an engaging platform for users to explore, discuss, and enjoy their preferred TV shows.',
+    view: 'https://jacintangwu.github.io/JavaScript-Capstone-Project/dist/',
+    source: 'https://github.com/JacintaNgwu/JavaScript-Capstone-Project.git',
+  },
+  {
+    imagsrc: Portfolio,
+    title: 'Portfolio',
+    text: 'Portfolio website showcases users skills, experience, and projects in a professional and minimalist design. Its responsive layout ensures it looks great on all devices. In addition, the website includes ways to connect with the user, allowing for easy communication and collaboration.',
+    view: 'https://jacintangwu.github.io/My-Portfolio/',
+    source: 'https://github.com/JacintaNgwu/My-Portfolio.git',
+  },
+];
+
+export default ProjectCardData;
